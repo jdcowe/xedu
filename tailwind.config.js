424f1d7311628bfdc465899d02e3f5fb90b571f0
jdcowe <jordancowe@gmail.com>
2021-02-18 21:21:28 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: '20px'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// tailwind.config.js
+module.exports = {
+  theme: {
+    // ...
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    // ...
+  ],
+}
